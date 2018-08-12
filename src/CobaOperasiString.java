13b47c13c26cf85deadf0e496b6bb05328249466
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-12 15:53:00 +0700

    Coba method substring,replace,& split

--- src/CobaOperasiString.java
+++ src/CobaOperasiString.java
@@ -59,6 +59,23 @@ public class CobaOperasiString {
         System.out.println("isi variable t tanpa spasi: \"" + t.trim() + "\"");
         System.out.println("gabungkan isi variable x dan r: " + x.concat(r));
         
+        System.out.println("karakter mulai posisi 3 di variable x: " + x.substring(3));
+        System.out.println("karakter dari posisi 3-5 di variable x: " + x.substring(3,6));
+        
+        String tclean = t.trim();
+        System.out.println("isi variable tclean: " + t.trim());
+        System.out.println("rubah 'i' jadi 'E' di variable tclean: " + tclean.replace("i","E"));
+        
+        String[] arrT = tclean.split(" ");
+        System.out.println("pecah kata di variable tclean: ");
+        for(int i=0;i<arrT.length;i++) 
+            System.out.println(arrT[i]);
+        
+        String[] arrTr = tclean.split("r");
+        System.out.println("pecah berdasarkan huruf 'r' di variable tclean: ");
+        for(int i = 0; i < arrTr.length; i++)
+         System.out.println(arrTr[i]);
+        
 }
 
 
