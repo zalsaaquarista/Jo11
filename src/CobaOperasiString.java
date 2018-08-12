4c622102468642dbdb406fd55c422416e1d7f61c
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-12 10:54:10 +0700

    Coba method equals & equalsIgnoreCase

--- src/CobaOperasiString.java
+++ src/CobaOperasiString.java
@@ -41,6 +41,16 @@ public class CobaOperasiString {
         System.out.println("isi x sama dengan r (Case Sensitive) : " + x.equals(r));
         System.out.println("isi x sama dengan r (Not Case Sensitive) : " + x.equalsIgnoreCase(r));
         
+        System.out.println("perbandingan isi x dengan y: " + x.compareTo(y));
+        System.out.println("perbandingan isi x denagn z (Case Sensitive): " + x.compareTo(z));
+        System.out.println("perbandingan isi x dengan r (Case Sensitive: " + x.compareTo(r));
+        System.out.println("perbandingan isi x dengan r (Not Case Sensitive): " + x.compareToIgnoreCase(r));
+        
+        String s = "operasi";
+        System.out.println("isi variable s: " + s);
+        System.out.println("perbandingan isi r dengan s (Case Sensitive): " + r.compareTo(s));
+        
+        
     }
             
 }
