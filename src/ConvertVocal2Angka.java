02b77c079de3b34d887a0d209a3a10069681ea59
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-12 15:26:28 +0700

    tambah konversi

--- src/ConvertVocal2Angka.java
+++ src/ConvertVocal2Angka.java
@@ -8,15 +8,35 @@
  *
  * @author ASUS
  */
+import java.util.Scanner;
 public class ConvertVocal2Angka {
-   private static void tampilJudul(String identitas)
-   {
+    private static void tampilJudul(String identitas)
+    {
        System.out.println("Identitas: " + identitas);
        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");       
-   }
-   public static void main(String[] args)
-   {
-       String identitas = "Zalsa Aquarista Kusumaningtyas / X RPL 3 / 39";
-       tampilJudul(identitas);
-   }
+    }
+  
+    private static String tampilJudul()
+    {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Masukkan Kalimat: ");
+       String kalimat = scanner.nextLine();
+       System.out.println("Kalimat Asli: " + kalimat);
+       
+       return kalimat;
+    }
+    private static String vocal2Angka(String kalimat)
+    {
+        char [][] arConvert =
+        {{'a', '4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i=0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        
+        return kalimat;
+    }
+  
+       
+   
 }
