78d4df94688f3b4946656731cf04abbc8b4df380
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-12 18:48:47 +0700

    Tambah Konversi

--- src/ConvertVocal2Angka.java
+++ src/ConvertVocal2Angka.java
@@ -24,4 +24,15 @@ public class ConvertVocal2Angka {
        
        return kalimat;
     }
+     private static String vocal2Angka(String kalimat)
+    {
+        char [][] arConvert =
+        {{'a', '4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i=0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        
+        return kalimat;
+    }
 }
