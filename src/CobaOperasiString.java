46732de38694df1e95b2d3cf7a70cad11f21109c
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-12 15:54:37 +0700

    Hasil method

--- src/CobaOperasiString.java
+++ src/CobaOperasiString.java
@@ -79,7 +79,7 @@ public class CobaOperasiString {
         double d = 1545454000;
         System.out.println(String.format("Uang saya %,.4f", d));
         System.out.println(String.format(Locale.forLanguageTag("in-ID"), "Uang saya Rp %,.2f", d));  
-        
+    }  
 }
 
 
