31d792afde86ca3f218050b2156ed5b911803df2
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-12 10:51:42 +0700

    Coba method length & isEmpty

--- src/CobaOperasiString.java
+++ src/CobaOperasiString.java
@@ -30,7 +30,16 @@ public class CobaOperasiString {
         System.out.println("isi variable y : " + (y));
         System.out.println("y adalah kosong : " + y.isEmpty());
         
+        System.out.println("isi x sama dengan y : " + x.equals(y));
         
+        String z = "operasi";
+        System.out.println("isi variable z: " + (z));
+        System.out.println("isi x sama dengan (Case Sensitive): " + x.equals(z));
+        
+        String r = "operasi";
+        System.out.println("isi variable r : " + (r));
+        System.out.println("isi x sama dengan r (Case Sensitive) : " + x.equals(r));
+        System.out.println("isi x sama dengan r (Not Case Sensitive) : " + x.equalsIgnoreCase(r));
         
     }
             
