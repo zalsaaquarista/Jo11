8f25c7fd43256bb8a2a7933b0d00ecfd3bf809ac
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-12 14:36:11 +0700

    Initial Commit

--- src/ConvertVocal2Angka.java
+++ src/ConvertVocal2Angka.java
@@ -8,29 +8,15 @@
  *
  * @author ASUS
  */
-import java.util.Scanner;
 public class ConvertVocal2Angka {
-    private static void tampilJudul(String[] args)
-    {
-       System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
-        
-       String identitas = "nama lengkap / kelas / no";
-       System.out.println("identitas: " + identitas); 
-    }
-    
-    private static String tampilInput()
-    {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Masukkan Kalimat: ");
-        String kalimat = scanner.nextLine();
-        System.out.println("Kalimat Asli: " + kalimat);
-        
-        return kalimat;
-    }
-    
-    private static String vocal2Angka(String kalimat)
-    {
-      
-    }
-    
-
+   private static void tampilJudul(String identitas)
+   {
+       System.out.println("Identitas: " + identitas);
+       System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");       
+   }
+   public static void main(String[] args)
+   {
+       String identitas = "Zalsa Aquarista Kusumaningtyas / X RPL 3 / 39";
+       tampilJudul(identitas);
+   }
+}
