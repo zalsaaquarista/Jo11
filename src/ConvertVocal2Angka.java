3d34ef3864b7a21027859a02e57a792bed7fe435
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-12 15:32:27 +0700

    Tambah Tampil Per Kata

--- src/ConvertVocal2Angka.java
+++ src/ConvertVocal2Angka.java
@@ -36,6 +36,14 @@ public class ConvertVocal2Angka {
         
         return kalimat;
     }
+    private static void tampilPerKata(String kalimat, String convert)
+    {
+        String[] arrKal = kalimat.split(" ");
+        String[] arrCon = convert.split(" ");
+        
+        for(int i=0;i<arrKal.length;i++)
+            System.out.println(arrKal[i]+" => "+arrCon[i]);
+    }
   
        
    
