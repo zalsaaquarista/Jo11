95780ca985271968eea64a67e1ad793152c83c82
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-12 10:50:16 +0700

    Initial Commit

--- src/CobaOperasiString.java
+++ src/CobaOperasiString.java
@@ -30,25 +30,6 @@ public class CobaOperasiString {
         System.out.println("isi variable y : " + (y));
         System.out.println("y adalah kosong : " + y.isEmpty());
         
-        System.out.println("isi x sama dengan y : " + x.equals(y));
-        
-        String z = "operasi";
-        System.out.println("isi variable z: " + (z));
-        System.out.println("isi x sama dengan (Case Sensitive): " + x.equals(z));
-        
-        String r = "operasi";
-        System.out.println("isi variable r : " + (r));
-        System.out.println("isi x sama dengan r (Case Sensitive) : " + x.equals(r));
-        System.out.println("isi x sama dengan r (Not Case Sensitive) : " + x.equalsIgnoreCase(r));
-        
-        System.out.println("perbandingan isi x dengan y: " + x.compareTo(y));
-        System.out.println("perbandingan isi x denagn z (Case Sensitive): " + x.compareTo(z));
-        System.out.println("perbandingan isi x dengan r (Case Sensitive: " + x.compareTo(r));
-        System.out.println("perbandingan isi x dengan r (Not Case Sensitive): " + x.compareToIgnoreCase(r));
-        
-        String s = "operasi";
-        System.out.println("isi variable s: " + s);
-        System.out.println("perbandingan isi r dengan s (Case Sensitive): " + r.compareTo(s));
         
         
     }
