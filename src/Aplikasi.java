a7f5a3eac8b16839a371da1c11bccadd01c476bd
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-08-15 13:13:00 +0700

    Hasil Aplikasi

--- src/Aplikasi.java
+++ src/Aplikasi.java
@@ -8,25 +8,57 @@
  *
  * @author ASUS
  */
+import java.util.Scanner;
 public class Aplikasi {
     public static void main(String[] args)
     {
        //Mengsisi Identitas
        String identitas = "Zalsa Aquarista Kusumaningtyas / X RPL 3 / 39";
        System.out.println("Identitas: " + identitas);
+       String u = "tidak" ;
+       String l ="ya" ;
+       Scanner baca = new Scanner(System.in);
+       String[] makan = new String[] {"Nasi pecel", "Gado-gado", "Nasi Campur", "Soto ayam", "Bakso"};
+       System.out.print("Masukkan jenis makanan: ");
+       String i = baca.nextLine();
+       int y = 0;
        
-       String y = "SMK TELKOM MALANG";
-       System.out.println("variable y: " + (y));
-       System.out.println("jumlah karakter: " + y.length());
-       System.out.println("jawaban y: " + y.isEmpty());
+       for(int x = 0; x < 5; x++)
+       {
+           if(i.equalsIgnoreCase(makan[x]))
+           {
+               y = 1;
+           }
        
-       String b = "SMPN 6 SIDOARJO";
-       System.out.println("variable b: " + (b));
-       System.out.println("jumlah karakter: " + b.length());
-       System.out.println("jawaban b: " + y.isEmpty());
+       }
        
-       System.out.println("Perbandingan jumlah y dan b: " + y.compareTo(b));
-       System.out.println("Perbedaan tulisan y dan b: " + y.toLowerCase());
-       System.out.println("Perbedaan tulisan b dan y: " + b.toLowerCase());      
-    }
+       if(y == 1)
+        {
+            System.out.println("Makanan yang dipesan tersedia");
+        }
+       
+        else
+        {
+            System.out.println("Makanan yang dipesan tidak tersedia");
+        }
+       
+       String r = "  ";
+       System.out.print("Apakah anda ingin memesan yang lain: ");
+       String jawaban = baca.nextLine();
+       
+       if(jawaban.equals(l))
+       {
+           System.out.print("Tuliskan pesanan yang anda ingin: ");
+           String pesanan = baca.nextLine();
+           System.out.println("Pesanan anda akan segera dilaksanakan");
+       }
+       else if(jawaban.equals(u))
+       {
+           System.out.println("Terima kasih karena telah memesan");
+       }
+       
+       System.out.println("Bandingkan karakter i dan r: " + i.compareTo(r));
+       System.out.println("Pada r terdapat huruf 'i': " + r.contains(r));
+       System.out.println("r adalah kosong: " + r.isEmpty());
+         }
 }
